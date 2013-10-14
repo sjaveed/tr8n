85e2ff2209fa9d8422f4c8d6018dfe50d32e19b3
Michael Berkovich <theiceberk@gmail.com>
2013-10-14 10:21:57 -0700

    migration fix

--- test/dummy/db/migrate/20131010005439_add_type_to_email_templates.rb
+++ test/dummy/db/migrate/20131010005439_add_type_to_email_templates.rb
@@ -1,7 +1,5 @@
 class AddTypeToEmailTemplates < ActiveRecord::Migration
   def up
-    remove_index :tr8n_email_templates, [:application_id, :keyword]
-
     rename_column :tr8n_email_templates, :body, :html_body
     add_column :tr8n_email_templates, :text_body, :text
     add_column :tr8n_email_templates, :type, :string
@@ -12,7 +10,5 @@ class AddTypeToEmailTemplates < ActiveRecord::Migration
     rename_column :tr8n_email_templates, :html_body, :body
     remove_column :tr8n_email_templates, :text_body
     remove_column :tr8n_email_templates, :type
-
-    add_index :tr8n_email_templates, [:type, :application_id, :keyword], :name => :tr8n_et_t_a
   end
 end
